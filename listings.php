88e01a6aaedcf5147c5c456c03a0aea3fddeaf50
Lauren Eldarazi <88990635+LaurenEld004@users.noreply.github.com>
2025-03-06 01:35:30 -0500

    Update listings.php

--- listings.php
+++ listings.php
@@ -1,3 +1,16 @@
+
+<?php
+    session_start();
+    if(isset($_SESSION['username'])){
+        $username = $_SESSION['username'];
+    }
+    else{
+        $username = "";
+    }
+?>
+
+
+
 <!--
     Listings page for HW1
 -->
