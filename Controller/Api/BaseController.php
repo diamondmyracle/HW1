4e59cb0f676503168526eff467637e2831d14005
Lauren Eldarazi <88990635+LaurenEld004@users.noreply.github.com>
2025-04-05 15:12:50 -0400

    Add files via upload

---
+++ Controller/Api/BaseController.php
@@ -0,0 +1,36 @@
+<?php
+    class BaseController
+    {
+        public function __call($name, $arguments)
+        {
+            $this->sendOutput('', array('404 Not Found')) ;
+        }
+
+        protected function getUriSegments()
+        {
+            $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ;
+            $uri = explode('/', $uri);
+
+            return $uri;
+        }
+
+        protected function getQueryStringParams()
+        {
+            return parse_str($_SERVER['QUERY_STRING'], $query) ;
+        }
+
+        protected function sendOutput($data, $httpHeaders=array())
+        {
+            header_remove('Set-Cokkie') ;
+
+            if (is_array($httpHeaders) && count($httpHeaders)) {
+                foreach ($httpHeaders as $httpHeader) {
+                    header($httpHeader) ;
+                }
+            }
+
+            echo $data ;
+            exit ;
+        }
+    }
+?>
