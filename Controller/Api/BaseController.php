920b200ae4b56aab692fdd71ecfe85234ad0930c
Spongebosch <minecraft.bosch@gmail.com>
2025-04-09 20:00:00 -0400

    small fix
    should be cookie not cokkie

--- Controller/Api/BaseController.php
+++ Controller/Api/BaseController.php
@@ -21,7 +21,7 @@
 
         protected function sendOutput($data, $httpHeaders=array())
         {
-            header_remove('Set-Cokkie') ;
+            header_remove('Set-Cookie') ;
 
             if (is_array($httpHeaders) && count($httpHeaders)) {
                 foreach ($httpHeaders as $httpHeader) {
