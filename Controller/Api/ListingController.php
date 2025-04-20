30ca089de99f477e560007030b6ba945097db0d8
Diamond <dbraddy@wesleyan.edu>
2025-04-20 15:26:22 -0400

    Update ListingController.php

--- Controller/Api/ListingController.php
+++ Controller/Api/ListingController.php
@@ -1,216 +1,233 @@
 <?php
-class UserController extends BaseController
+class ListingController extends BaseController
 {
+    public function listingByID()
+    {
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+
+        if (strtoupper($requestMethod) == 'POST') {
+            try {
+                $json = file_get_contents('php://input');
+                $data = json_decode($json, true);
+
+                $listingModel = new ListingModel();
+                $listing = $listingModel->getListingByID($data);
+                $responseData = json_encode($listing);
+
+            } catch (Error $e) {
+                $strErrorDesc = $e->getMessage() . ' Something went wrong! Please contact support.';
+                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } else {
+            $strErrorDesc = 'Method not supported';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+
+        // send output
+        if (!$strErrorDesc) {
+            $this->sendOutput(
+                $responseData,
+                ['Content-Type: application/json', 'HTTP/1.1 200 OK']
+            );
+        } else {
+            $this->sendOutput(
+                json_encode(['error' => $strErrorDesc]),
+                ['Content-Type: application/json', $strErrorHeader]
+            );
+        }
+    }
+
+
     /** 
-* "/user/list" Endpoint - Get list of users 
-*/
+     * "/listing/list" Endpoint - Get list of listings 
+     */
     public function listAction()
     {
         $strErrorDesc = '';
         $requestMethod = $_SERVER["REQUEST_METHOD"];
         $arrQueryStringParams = $this->getQueryStringParams();
+
         if (strtoupper($requestMethod) == 'GET') {
             try {
-                $userModel = new UserModel();
-                $intLimit = 10;
+                $listingModel = new ListingModel();
+                $intLimit = 100;
                 if (isset($arrQueryStringParams['limit']) && $arrQueryStringParams['limit']) {
                     $intLimit = $arrQueryStringParams['limit'];
                 }
-                $arrUsers = $userModel->getUsers($intLimit);
-                $responseData = json_encode($arrUsers);
+                $arrListings = $listingModel->getListings($intLimit);
+                $responseData = json_encode($arrListings);
+
             } catch (Error $e) {
-                $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+                $strErrorDesc = $e->getMessage() . ' Something went wrong! Please contact support.';
                 $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
             }
         } else {
             $strErrorDesc = 'Method not supported';
             $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
         }
-        /* // send output 
+
+        // send output
         if (!$strErrorDesc) {
             $this->sendOutput(
                 $responseData,
-                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+                ['Content-Type: application/json', 'HTTP/1.1 200 OK']
             );
         } else {
-            $this->sendOutput(json_encode(array('error' => $strErrorDesc)), 
-                array('Content-Type: application/json', $strErrorHeader)
+            $this->sendOutput(
+                json_encode(['error' => $strErrorDesc]),
+                ['Content-Type: application/json', $strErrorHeader]
             );
         }
-        */
     }
 
-public function userExists()
-    {
-        $strErrorDesc = '' ;
+public function createAction()
+{
+    $requestMethod = $_SERVER["REQUEST_METHOD"];
+
+    if (strtoupper($requestMethod) === 'POST') {
         try {
-            $userModel = new UserModel() ;
+            $listingModel = new ListingModel();
 
-            $json = file_get_contents("php://input") ;
-            $data = json_decode($json, true) ;
+            $json = file_get_contents('php://input');
+            $data = json_decode($json, true);
 
-            $username = $data["username"] ;
+            $result = $listingModel->createListing($data);
 
-            $result = $userModel->usernameInDatabase($username) ;
-            $responseData = json_encode($result) ;
-        } catch (Error $e) {
-            $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
-            $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
-        }
-        // send output 
-        if (!$strErrorDesc) {
-            $this->sendOutput(
-                json_encode(
-                    array_merge(
-                        ["status" => "success"],
-                        ["data" => json_decode($responseData)]
-                    )),
-                array('Content-Type: application/json', 'HTTP/1.1 201 OK')
-            );
-        } else {
-            $this->sendOutput(
-                json_encode([
-                    "status" => "error",
-                    ["error" => $strErrorDesc]
-                ]),
-                array('Content-Type: application/json', $strErrorHeader)
-            );
-        }
-    }
-
-    public function createUser()
-    {
-        $strErrorDesc = '' ;
-        $requestMethod = $_SERVER["REQUEST_METHOD"] ;
-        if (strtoupper($requestMethod) == 'POST') {
-            try {
-                $userModel = new UserModel() ;
-
-                $json = file_get_contents("php://input") ;
-                $data = json_decode($json, true) ;
-
-                $username = $data["username"] ;
-                $password = $data["psw"] ;
-
-                $hashedPassword = password_hash($password, PASSWORD_DEFAULT) ;
-                $result = $userModel->insertUser($username, $hashedPassword) ;
-                $responseData = json_encode($result) ;
-            } catch (Error $e) {
-                $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
-                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            if ($result) {
+                $this->sendOutput(json_encode([
+                    'status' => 'success',
+                    'message' => 'Listing created'
+                ]), ['Content-Type: application/json', 'HTTP/1.1 201 Created']);
+            } else {
+                $this->sendOutput(json_encode([
+                    'status' => 'error',
+                    'message' => 'Failed to create listing.'
+                ]), ['Content-Type: application/json', 'HTTP/1.1 500 Internal Server Error']);
             }
-        } else {
-            $strErrorDesc = 'Method not supported';
-            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        } catch (Exception $e) {
+            $this->sendOutput(json_encode([
+                'status' => 'error',
+                'message' => 'Exception: ' . $e->getMessage()
+            ]), ['Content-Type: application/json', 'HTTP/1.1 500 Internal Server Error']);
         }
-        // send output 
-        if (!$strErrorDesc) {
+    } else {
+        $this->sendOutput(json_encode([
+            'status' => 'error',
+            'message' => 'Method not allowed'
+        ]), ['Content-Type: application/json', 'HTTP/1.1 405 Method Not Allowed']);
+    }
+}
+
+    
+
+public function updateAction()
+{
+    $requestMethod = $_SERVER["REQUEST_METHOD"];
+
+    if (strtoupper($requestMethod) === 'PUT') {
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        try {
+            $listingModel = new ListingModel();
+            $result = $listingModel->updateListing($data);
+
+            
+            if ($result) {
+                $this->sendOutput(
+                    json_encode([
+                        'success' => true,
+                        'message' => 'Listing updated'
+                    ]),
+                    ['Content-Type: application/json']
+                );
+            } else {
+                $this->sendOutput(
+                    json_encode([
+                        'success' => false,
+                        'message' => 'No changes made or listing not found.'
+                    ]),
+                    ['Content-Type: application/json', 'HTTP/1.1 404 Not Found']
+                );
+            }
+            
+
+        } catch (Exception $e) {
+            
             $this->sendOutput(
                 json_encode([
-                    "status" => "success",
-                    "message" => "User has been created."
+                    'success' => false,
+                    'message' => 'Exception: ' . $e->getMessage()
                 ]),
-                array('Content-Type: application/json', 'HTTP/1.1 201 OK')
+                ['Content-Type: application/json', 'HTTP/1.1 500 Internal Server Error']
             );
-        } else {
-            $this->sendOutput(
-                json_encode([
-                    "status" => "error",
-                    ["error" => $strErrorDesc]
-                ]),
-                array('Content-Type: application/json', $strErrorHeader)
-            );
+            
         }
+    } else {
+        
+        $this->sendOutput(
+            json_encode([
+                'success' => false,
+                'message' => 'Method not allowed'
+            ]),
+            ['Content-Type: application/json', 'HTTP/1.1 405 Method Not Allowed']
+        );
         
     }
+}
 
-    public function getUserData() 
-    {
-        $strErrorDesc = '' ;
+public function deleteAction()
+{
+    $requestMethod = $_SERVER["REQUEST_METHOD"];
+
+    if (strtoupper($requestMethod) === 'DELETE') {
+        $data = json_decode(file_get_contents('php://input'), true);
+
         try {
-            $userModel = new UserModel() ;
+            $listingModel = new ListingModel();
+            $result = $listingModel->deleteListing($data);
 
-            $json = file_get_contents("php://input") ;
-            $data = json_decode($json, true) ;
-
-            $username = $data["username"] ;
-
-            $result = $userModel->getUserInfo($username) ;
-            $responseData = json_encode($result) ;
-        } catch (Error $e) {
-            $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
-            $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
-        }
-        // send output 
-        if (!$strErrorDesc) {
-            $this->sendOutput(
-                json_encode(
-                    array_merge(
-                        ["status" => "success"],
-                        ["data" => json_decode($responseData)]
-                    )),
-                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
-            );
-        } else {
-            $this->sendOutput(
-                json_encode([
-                    "status" => "error",
-                    ["error" => $strErrorDesc]
-                ]),
-                array('Content-Type: application/json', $strErrorHeader)
-            );
-        }
-    }
-
-    public function loginUser()
-    {
-        $strErrorDesc = '' ;
-        try {
-            $userModel = new UserModel() ;
-
-            $json = file_get_contents("php://input") ;
-            $data = json_decode($json, true) ;
-
-            $username = $data["username"] ;
-            $password = $data["psw"] ;
-
-            $message = "Invalid username or password." ;
-            $status = "failure" ;
-
-            $result = $userModel->getUserInfo($username) ;
-            if (count($result) > 0) {
-                $row = $result[0] ;
-                $hashedPassword = $row["password"] ;
-
-                if (password_verify($password, $hashedPassword)) {
-                    $_SESSION["loggedin"] = true ;
-                    $_SESSION["username"] = $username ;
-                    $message = "Logged in." ;
-                    $status = "success" ;
-                }
+            
+            if ($result) {
+                $this->sendOutput(
+                    json_encode([
+                        'success' => true,
+                        'message' => 'Listing deleted'
+                    ]),
+                    ['Content-Type: application/json']
+                );
+            } else {
+                $this->sendOutput(
+                    json_encode([
+                        'success' => false,
+                        'message' => 'Listing not found'
+                    ]),
+                    ['Content-Type: application/json', 'HTTP/1.1 404 Not Found']
+                );
             }
-        } catch (Error $e) {
-            $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
-            $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
-        }
-        // send output 
-        if (!$strErrorDesc) {
+            
+
+        } catch (Exception $e) {
+            
             $this->sendOutput(
                 json_encode([
-                    "status" => $status,
-                    "data" => $message
+                    'success' => false,
+                    'message' => 'Exception: ' . $e->getMessage()
                 ]),
-                array('Content-Type: application/json', 'HTTP/1.1 201 OK')
+                ['Content-Type: application/json', 'HTTP/1.1 500 Internal Server Error']
             );
-        } else {
-            $this->sendOutput(
-                json_encode([
-                    "status" => "error",
-                    ["error" => $strErrorDesc]
-                ]),
-                array('Content-Type: application/json', $strErrorHeader)
-            );
+            
         }
+    } else {
+        
+        $this->sendOutput(
+            json_encode([
+                'success' => false,
+                'message' => 'Method not allowed'
+            ]),
+            ['Content-Type: application/json', 'HTTP/1.1 405 Method Not Allowed']
+        );
+        
     }
 }
 
