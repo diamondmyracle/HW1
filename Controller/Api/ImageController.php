2dd4fd6a46cc5081e27307b3f00b09855f2a9366
Spongebosch <minecraft.bosch@gmail.com>
2025-04-23 22:55:32 -0400

    Image upload implemented through API (Issue #254)
    I've implemented image upload through the rest api. You send the image data in a base 64 encoding in a JSON body.

--- Controller/Api/ImageController.php
+++ Controller/Api/ImageController.php
@@ -6,17 +6,42 @@
             $strErrorDesc = '' ;
             $requestMethod = $_SERVER["REQUEST_METHOD"] ;
             if (strtoupper($requestMethod) == 'POST') {
-                try {
+                try 
+                {
                     $json = file_get_contents("php://input") ;
                     $data = json_decode($json, true) ;
 
+                    $imageName = $data["name"] ;
                     $imageBase64 = $data["image"] ;
+
+                    if (strpos($imageBase64, 'base64,') !== false) {
+                        $imageBase64 = explode('base64,', $imageBase64)[1] ;
+                    }
                     $decodedImage = base64_decode($imageBase64) ;
 
-                    //$allowedExtensions = ['jpg', 'jpeg', 'png'] ;
-                    //$fileExtension = strtolower(pathinfo($decodedImage['name'], PATHINFO_EXTENSION)) ;
+                    $allowedExtensions = ['jpg', 'jpeg', 'png'] ;
+                    $fileExtension = strtolower(pathinfo($imageName, PATHINFO_EXTENSION)) ;
 
+                    if (!in_array($fileExtension, $allowedExtensions)) {
+                        $error = "Invalid file type. Only JPG, JPEG, and PNG files are allowed." ;
+                        $strErrorHeader = 'HTTP/1.1 500 Internal Server Error' ;
+                        $this->sendOutput(
+                            json_encode(['error' => $error]),
+                            ['Content-Type: application/json', $strErrorHeader]
+                        ) ;
+                        return ;
+                    } else {
+                        $imageFilename = uniqid("upload_") ;
+                        $targetDir = PROJECT_ROOT_PATH . "/uploads/" ;
+                        $targetFilePath = $targetDir . $imageFilename . "." . $fileExtension ;
 
+                        // // Ensure uploads directory exists
+                        // if (!is_dir($targetDir)) {
+                        //     mkdir($targetDir, 0755, true) ;
+                        // }
+
+                        file_put_contents($targetFilePath, $decodedImage) ;
+                    }
                 } catch (Error $e) {
                     $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
                     $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
